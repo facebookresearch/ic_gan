4287459314952c955c9ae3d35baaa89fec3badee
ArantxaCasanova <20665200+ArantxaCasanova@users.noreply.github.com>
2021-06-14 14:31:14 -0700

    reproducibility logs and scripts

--- BigGAN-PyTorch/scripts/utils/prepare_data.sh
+++ BigGAN-PyTorch/scripts/utils/prepare_data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#python make_hdf5.py --dataset I128 --batch_size 256 --data_root data
-#python calculate_inception_moments.py --dataset I128_hdf5 --data_root data
 
-
-#python make_hdf5.py --resolution 64 --split 'train' --data_root 'data/Imagenet_all/' --out_path 'mock_data'
-
-#python make_hdf5.py --resolution 64 --split 'train' --data_root '../../anyshot_longtail/data/Imagenet_all/' --out_path 'mock_data' --longtail
-#python make_hdf5.py --resolution 64 --split 'train' --data_root '../../anyshot_longtail/data/Imagenet_all/' --out_path 'mock_data'
-
-python calculate_inception_moments.py --resolution 64 --split 'train' --data_root 'mock_data' --longtail --load_in_mem
+# ImageNet
+python make_hdf5.py --resolution 64 --split 'train' --data_root '../../anyshot_longtail/data/Imagenet_all/' --out_path 'mock_data'
 python calculate_inception_moments.py --resolution 64 --split 'train' --data_root 'mock_data' --load_in_mem --out_path 'mock_data'
+python make_hdf5.py --resolution 64 --split 'val' --data_root '../../anyshot_longtail/data/Imagenet_all/' --out_path 'mock_data'
+python calculate_inception_moments.py --resolution 64 --split 'val' --data_root 'mock_data' --load_in_mem --out_path 'mock_data'
+
+# ImageNet-LT
+python make_hdf5.py --resolution 64 --split 'train' --data_root '../../anyshot_longtail/data/Imagenet_all/' --out_path 'mock_data' --longtail
+python calculate_inception_moments.py --resolution 64 --split 'train' --data_root 'mock_data' --longtail --load_in_mem
+python make_hdf5.py --resolution 64 --split 'val' --data_root '../../anyshot_longtail/data/Imagenet_all/' --out_path 'mock_data'
+python calculate_inception_moments.py --resolution 64 --split 'val' --data_root 'mock_data' --load_in_mem --out_path 'mock_data' --stratified_moments
